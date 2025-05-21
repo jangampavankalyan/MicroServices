65562979f3927050ba2303f301b8b05a965365d7
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-05-21 12:03:06 +0530

    End of Inventory Service

---
+++ Inventory-service/src/main/java/com/example/Inventory_service/service/InventoryService.java
@@ -0,0 +1,17 @@
+package com.example.Inventory_service.service;
+
+import com.example.Inventory_service.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+
+    @Autowired
+    InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity) {
+        // FInd an inventory for a given skuCode where quantity >= 0
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode,quantity);
+    }
+}
