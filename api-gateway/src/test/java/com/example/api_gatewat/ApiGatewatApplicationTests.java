3dd80fa59fc0ef974164d192d66aabb8382699c2
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-05-26 20:47:24 +0530

    Start of API Gateway

---
+++ api-gateway/src/test/java/com/example/api_gatewat/ApiGatewatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gatewat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
